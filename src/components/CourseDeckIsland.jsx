14353ab27f38f8e6007d4d28656aa400a0be4ce4
Kim Jeongjin <kimjj81@gmail.com>
2026-04-10 18:07:15 +0900

    fix: avoid serializing course components into islands

--- src/components/CourseDeckIsland.jsx
+++ src/components/CourseDeckIsland.jsx
@@ -1,7 +1,14 @@
 import CourseDeck from './CourseDeck';
+import { getCourseBySlug } from '../courses/registry';
 import { LocaleProvider } from '../i18n/LocaleContext';
 
-export default function CourseDeckIsland({ course, initialLocale = 'ko', initialPathname = '/' }) {
+export default function CourseDeckIsland({ courseSlug, initialLocale = 'ko', initialPathname = '/' }) {
+  const course = getCourseBySlug(courseSlug);
+
+  if (!course) {
+    throw new Error(`Unknown course slug: ${courseSlug}`);
+  }
+
   return (
     <LocaleProvider initialLocale={initialLocale} initialPathname={initialPathname}>
       <CourseDeck course={course} />
--- src/components/CourseOverviewIsland.jsx
+++ src/components/CourseOverviewIsland.jsx
@@ -1,7 +1,20 @@
-import CourseOverviewPage from '../react-pages/CourseOverviewPage';
+import { getCourseDetailBySlug } from '../course-details/registry';
+import { getCourseBySlug } from '../courses/registry';
 import { LocaleProvider } from '../i18n/LocaleContext';
+import CourseOverviewPage from '../react-pages/CourseOverviewPage';
 
-export default function CourseOverviewIsland({ course, detail, initialLocale = 'ko', initialPathname = '/' }) {
+export default function CourseOverviewIsland({ courseSlug, initialLocale = 'ko', initialPathname = '/' }) {
+  const course = getCourseBySlug(courseSlug);
+  const detail = getCourseDetailBySlug(courseSlug);
+
+  if (!course) {
+    throw new Error(`Unknown course slug: ${courseSlug}`);
+  }
+
+  if (!detail) {
+    throw new Error(`Unknown course detail slug: ${courseSlug}`);
+  }
+
   return (
     <LocaleProvider initialLocale={initialLocale} initialPathname={initialPathname}>
       <CourseOverviewPage course={course} detail={detail} />
